fe0dae0c25774e624d273c2f69fbb07db50af978
ZFail <zfail0@gmail.com>
2020-12-02 08:33:34 +0300

    tab, list

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,46 @@
-import "./polyfills";
+import './polyfills';
 
-import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatNativeDateModule } from "@angular/material/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { DemoMaterialModule } from "./app/material-module";
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material/form-field";
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './app/material-module';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
 
-import { AppComponent } from "./app/app-component";
-import { ButtonsComponent } from "./app/components/buttons/buttons";
-import { AllComponent } from "./app/components/all/all.component";
-import { ChipsComponent } from "./app/components/chips/chips.component";
+import {AppComponent} from './app/app-component';
+import {ButtonsComponent} from './app/components/buttons/buttons';
+import {AllComponent} from './app/components/all/all.component';
+import {ChipsComponent} from './app/components/chips/chips.component';
+import {ListComponent} from './app/components/list/list.component';
 
 // Default MatFormField appearance to 'fill' as that is the new recommended approach and the
 // `legacy` and `standard` appearances are scheduled for deprecation in version 10.
 // This makes the examples that use MatFormField render the same in StackBlitz as on the docs site.
 @NgModule({
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpClientModule,
-    DemoMaterialModule,
-    MatNativeDateModule,
-    ReactiveFormsModule
-  ],
-  entryComponents: [AppComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        HttpClientModule,
+        DemoMaterialModule,
+        MatNativeDateModule,
+        ReactiveFormsModule
+    ],
+    entryComponents: [AppComponent],
 
-  declarations: [AppComponent, ButtonsComponent, AllComponent, ChipsComponent],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, ButtonsComponent, AllComponent, ChipsComponent, ListComponent],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+    .bootstrapModule(AppModule)
+    .catch(err => console.error(err));
 
 /**  Copyright 2020 Google LLC. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at http://angular.io/license */
+ Use of this source code is governed by an MIT-style license that
+ can be found in the LICENSE file at http://angular.io/license */
