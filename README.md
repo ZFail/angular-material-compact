13bde7864429f589151f04cb1445282db6e1b9cc
ZFail <zfail0@gmail.com>
2020-12-02 03:25:27 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-material-compact
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-material-compact)
---
+++ src/app/select-custom-trigger-example.ts
@@ -0,0 +1,23 @@
+import {Component} from '@angular/core';
+import {FormControl} from '@angular/forms';
+
+/** @title Select with custom trigger text */
+@Component({
+  selector: 'select-custom-trigger-example',
+  templateUrl: 'select-custom-trigger-example.html',
+  styleUrls: ['select-custom-trigger-example.css'],
+})
+export class SelectCustomTriggerExample {
+  toppings = new FormControl();
+
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+
+  constructor() {
+    this.toppings.setValue(['Onion', 'Pepperoni'])
+  }
+}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
